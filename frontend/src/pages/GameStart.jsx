d894d3cf1e8438b1ee78f4bb085d083ac7c698cb
MidNightRoad <bergerpierre09@gmail.com>
2022-04-02 17:06:51 +0200

    work on gamestart style

--- frontend/src/pages/GameStart.jsx
+++ frontend/src/pages/GameStart.jsx
@@ -6,15 +6,32 @@ function GameStart() {
       <div className="container">
         <div className="playerlist">
           <div className="list">
-            <ul className="listp" />
-              <li />
-              <li />
-              <li />
-              <li />
-            
+            <h2 className="title-list">Liste des joueurs:</h2>
+            <ul className="listp">
+              <li>Michel Tacos</li>
+              <hr className="ligne" />
+              <li>Michel Tacos</li>
+              <hr className="ligne" />
+              <li>Michel Tacos</li>
+              <hr className="ligne" />
+              <li>Michel Tacos</li>
+              <hr className="ligne" />
+              <li>Michel Tacos</li>
+            </ul>
+            <button className="btn-start">Lancer la partie</button>
           </div>
         </div>
-        <div className="rules"></div>
+        <div className="rules">
+          <div className="container-link">
+            <h2>Lien de la partie:</h2>
+          </div>
+          <div className="rules-list">
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          </div>
+        </div>
       </div>
     </div>
   );
